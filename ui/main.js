1dbffc0f479b3c0131f320348bac572cba0c56e2
ansonfertal <ans5tr@gmail.com>
2017-02-11 22:19:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "Welcome to my App";
 var img = document.getElementById("ian");
+var marginLeft = 0;
 img.onclick = function() {
-    img.style.marginLeft = "100px";
+    var interval = setInterval(moveRight, 50);
 };
+    function moveRight () {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + "px";
+}
