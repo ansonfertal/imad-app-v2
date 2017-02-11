8259b50bf82a520b66328f780db39b7b35ff6384
ansonfertal <ans5tr@gmail.com>
2017-02-11 23:42:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,9 +102,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:article-one', function(req, res) {
-    // var articleName = req.param.articleName;
-    res.send(createTemplate(article-one));
+app.get('/:articleName', function(req, res) {
+    var articleName = req.param.articleName;
+    res.send(createTemplate(articleName));
 });
 
 app.get('/ui/style.css', function (req, res) {
