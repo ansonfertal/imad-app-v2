3f64daf6ae6a2b8376686b4a849bd498dbc413e7
ansonfertal <ans5tr@gmail.com>
2017-02-11 22:06:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,5 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "Welcome to my App";
+var img = document.getElementById("ian");
+img.style.marginLeft = "100px";
