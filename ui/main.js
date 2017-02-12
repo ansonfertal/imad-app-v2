9dd4e14f1545d10d5862e0964939d8e5c5e32aa6
ansonfertal <ans5tr@gmail.com>
2017-02-12 22:22:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "Welcome to my App";
-var img = document.getElementById("ian");
-var marginLeft = 0;
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+//Counter code
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+    
 };
-    function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + "px";
-}
