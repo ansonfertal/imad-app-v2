aa784a1dd8ba77d7b46cd1b31091d4bed0ead45f
ansonfertal <ans5tr@gmail.com>
2017-02-12 22:44:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,23 @@
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
     
+    //Create a request object
+    var request = new XMLhttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onsteadystatechange = function()
+    {
+        if(request.readyState == XMLhttpRequest.DONE) {
+            if(request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //Make a request
+    request.open('GET', 'http://ansonfertal.imad.hasura-app.io/counter', true);
+    request.send(null);
+
 };
