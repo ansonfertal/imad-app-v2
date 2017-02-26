8ddb89ee8df91c6a58ebfffd2e01bfe091125dc8
ansonfertal <ans5tr@gmail.com>
2017-02-27 05:12:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article one under construction');
+  res.send('Article two under construction');
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article one under construction');
+  res.send('Article ethre under construction');
 });
 
 app.get('/ui/style.css', function (req, res) {
