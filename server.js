f5900cad5a1681682a733b4c4c9bf4b2e90cef19
ansonfertal <ans5tr@gmail.com>
2017-03-05 00:41:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,20 +105,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/test-db', function(req, res) {
-    //make a select request
-    //return the response with the results
-    
-    var pool = new Pool(config);
-pool.query('SELECT * FROM test', function(err, result) {
-    if(err) {
-        res.status(500).send(err.toString());
-    } else {
-        res.send(JSON.stringify(result.rows));
-    }
-});
-    
-});
 
 app.get('/articles/:articleName', function (req, res) {
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
