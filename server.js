b6ac2e252c2eab7a6c27ab2cb39d01235cdba205
ansonfertal <ans5tr@gmail.com>
2017-03-05 00:43:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,13 +111,15 @@ app.get('/articles/:articleName', function (req, res) {
         
         if(err) {
             res.status(500).send(err.toString());
-        } else if(result.rows.length === 0) {
-            result.status(404).send('Article not found');
+        } else {
+        
+            if(result.rows.length === 0) {
+                result.status(404).send('Article not found');
         } else {
             var articleData = result.rows[0];
             res.send(createTemplate(articleData));
         }
-            
+    }
         
     });
 
